08cdafb8c2463e33bfd25d273b4436b25b94fbe5
Rʟxғʟʏ⁴̅⁰͍⁴̵ <87053694+Rlxfly@users.noreply.github.com>
2022-05-30 20:04:07 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,75 @@
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import { watchFile, unwatchFile, readFileSync } from 'fs'
+
+global.owner = 
+ [
+  ['+62 838-2007-3017'],
+  ['+62 878-2765-9529'],
+  ['+62 852-4075-0713']
+]
+
+
+
+ // ['6283820073017', '436506665652696', '6285240750713']
+
+  // [number, dia creator/owner?, dia developer?]
+// Put your number here
+global.mods = [] // Want some help?
+global.prems = [] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  pencarikode: 'https://pencarikode.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  violetics: 'https://violetics.pw',
+  rrul: 'https://api-rull.herokuapp.com',
+  hadi: 'http://hadi-api.herokuapp.com'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll',
+  'https://violetics.pw': 'beta'
+}
+
+global.thumbnailUrl = [
+  'https://telegra.ph/file/81260a8b9e8cff26d2b48.jpg', 'https://telegra.ph/file/ac4928f0824a2a0492737.jpg',
+  'https://telegra.ph/file/6359b013bc7e52c3b346f.jpg', 'https://telegra.ph/file/d43c89a5d2da72875ec05.jpg',
+  'https://telegra.ph/file/7d6c0e35f9c8f52715541.jpg', 'https://telegra.ph/file/ef4b742d47e6a9115e2ff.jpg',
+  'https://telegra.ph/file/55e5af5f33fbd57104187.jpg', 'https://telegra.ph/file/af236598456b95884bd15.jpg',
+  'https://telegra.ph/file/de92ed4a729887ffc974c.jpg', 'https://telegra.ph/file/00ce42a193b1dbbf907d4.jpg'
+]
+
+// Sticker WM
+global.packname = 'WhatsAppCode'
+global.author = ''
+global.thumb = readFileSync('./me.png')
+global.multiplier = 69 // The higher, The harder levelup
+
+//Text here
+global.me = 'Cʀᴇᴀᴛᴇᴅ Bʏ Ꭱʟxғʟʏ - 2022'
+global.str = '-------------'
+
+
+
+global.messageTemplate = ["Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}"]
+var a = "UCxxnxya_32jcKj4yN1_kD7A"
+let b = Math.floor(Math.random()*a.length)
+global.channel_id = a
+
+
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
---
+++ ytconf.js
@@ -0,0 +1,2 @@
+global.messageTemplate: "Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}"
+global.channel_id: "UC6Gnq8egewfIKibLzkr_Lng"
