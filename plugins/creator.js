633d24932411b3cbbd155a27ff5cc4d389c92a0d
Rʟxғʟʏ⁴̅⁰͍⁴̵ <87053694+Rlxfly@users.noreply.github.com>
2022-06-09 20:45:08 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,12 @@
 function handler(m) {
+  
   const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+
+  let i = global.owner.map(([number]) => number).map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net')
+  
+  
+  this.sendContact(m.chat, data.map(([id, name]) => [id, this.getName(i[0])]), m)
+  
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
@@ -8,3 +14,4 @@ handler.tags = ['info']
 handler.command = /^(owner|creator)$/i
 
 export default handler
+
