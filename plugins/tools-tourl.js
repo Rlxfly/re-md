abd8f6d3abc77104031d68fd87bc8a9ee270c09b
Rʟxғʟʏ⁴̅⁰͍⁴̵ <87053694+Rlxfly@users.noreply.github.com>
2022-06-09 15:18:06 +0700

    Update tools-tourl.js

--- plugins/tools-tourl.js
+++ plugins/tools-tourl.js
@@ -1,28 +1,33 @@
-import axios from 'axios'
-import clph from 'caliph-api'
-import uploadImage from '../lib/uploadImage.js'
+import fs from 'fs'
+import fetch from 'node-fetch'
+import FormData from 'form-data'
 
 let handler = async (m) => {
 	let q = m.quoted ? m.quoted : m
-	let mime = (q.msg || q).mimetype || q.mediaType || ''
-	let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
-	let media = await q.download()
-	if (isTele && media.length < 5242880) {
-		let data = await uploadImage(media)
-		m.reply(data)
-	} else if (/image|video|audio|sticker|document/.test(mime)) {
-		let data = await clph.tools.uploadFile(media),
-			shorten = await shortUrl(data.result.url)
-		m.reply(shorten)
+	let mime = q.mediaType || ''
+	if (/image|video|audio|sticker|document/.test(mime)) {
+		let media = await q.download(true)
+		let data = await uploadFile(media)
+		m.reply(data.files[0].url)
 	} else throw 'No media found'
 }
+
 handler.help = ['tourl']
 handler.tags = ['tools']
-handler.command = /^(upload|tourl)$/i
+handler.command = /^(upload|up|tourl)$/i
 
 export default handler
 
-async function shortUrl(url) {
-	let res = await axios.post('https://short.up.railway.app/api/links', { content: url })
-	return res.data.link
+async function uploadFile(path) {
+	let form = new FormData()
+	form.append('files[]', fs.createReadStream(path))
+	let res = await (await fetch('https://uguu.se/upload.php', {
+		method: 'post',
+		headers: {
+			...form.getHeaders()
+		},
+		body: form
+	})).json()
+	await fs.promises.unlink(path)
+	return res
 }
