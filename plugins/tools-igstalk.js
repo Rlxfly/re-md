73331dbb369d695651be6aaa24b0a3c00abdfdc9
Rʟxғʟʏ⁴̅⁰͍⁴̵ <87053694+Rlxfly@users.noreply.github.com>
2022-06-10 13:20:42 +0700

    Update tools-igstalk.js

--- plugins/tools-igstalk.js
+++ plugins/tools-igstalk.js
@@ -1,19 +1,44 @@
-import fetch from 'node-fetch'
+import { instagramStalk } from '@bochilteam/scraper'
 
-let handler = async (m, { conn, text }) => {
-	if (!text) throw 'Input Username'
-	text = text.replace(/^@/, '')
-	let res = await fetch(API('https://expressjs-akkun.up.railway.app', '/instagram/stalk', { user: text }))
-	if (!res.ok) throw await res.text()
-	let json = await res.json()
-	if (!json.success) throw json.msg
-	let data = json.result, img = data['hd_profile_pic_url_info'].url;
-	['id', 'hd_profile_pic_url_info', 'account_type'].forEach(Reflect.deleteProperty.bind(null, data))
-	let txt = Object.keys(data).map((v) => `*${v.replace(/[_]/g, ' ').capitalize()}:* ${data[v]}`).join`\n`
-	conn.sendMessage(m.chat, { image: { url: img }, caption: txt.trim() }, { quoted: m })
+let handler= async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Example use ${usedPrefix}${command} <username>`
+    const {
+        username,
+      avatar,
+        name,
+        description,
+        followersH,
+        followingH,
+        postsH,
+    } = await instagramStalk(args[0])
+
+  let data = `
+${username} » 「 ${name} 」
+
+${followersH}  Fᴏʟʟᴏᴡᴇʀꜱ
+${followingH}  Fᴏʟʟᴏᴡɪɴɢ
+${postsH} Pᴏꜱᴛ
+Bɪᴏ: ${description}
+`.trim()
+
+  let pp = await( await conn.getFile(avatar)).data
+  
+  conn.sendHydrated(m.chat, '「  𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢 𝙎𝙩𝙖𝙡𝙠 」' , data, pp, `https://instagram.com/${username.replace(/^@/, '')}`, 'Link Profile', '0', 'Nothing', [
+
+      [null],
+
+      [null],
+
+      [null, null]
+
+    ], null,  { asLocation: true })
 }
-handler.help = ['igstalk']
+
+handler.help = ['igstalk'].map(v => v + ' <username>')
 handler.tags = ['tools']
-handler.command = /^igstalk|stalkig$/i
+
+handler.command = /^(igstalk)$/i
 
 export default handler
+
+//Kalo mau ambil start/follow dlu :v
