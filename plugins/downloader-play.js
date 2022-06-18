7450ce1089053ca7dd267567bbcc9e78ec46e830
LitRHap <73509282+LitRHap@users.noreply.github.com>
2022-06-18 14:40:59 +0700

    Update downloader-play.js

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, text, usedPrefix }) => {
 	let vid = await (await fetch(API('rrul', '/api/yt/yts', { q: text }))).json()
 	if (!vid.result.length) throw `Query "${query}" Not Found :/`
 	let { url, title, description, image, seconds, timestamp, ago, views } = vid.result[0]
-	let ytLink = `https://yt-downloader.akkun3704.repl.co/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
+	let ytLink = `https://yt-downloader.aliefputra.repl.co/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
 	let capt = `*Title:* ${title}\n*Published:* ${ago}\n*Duration:* ${timestamp}\n*Views:* ${views.toLocaleString()}\n*Url:* ${url}`
 	let buttons = [{ buttonText: { displayText: 'Video' }, buttonId: `${usedPrefix}ytv ${url} 360` }]
 	let msg = await conn.sendMessage(m.chat, { image: { url: image }, caption: capt, footer: '_Audio on progress..._', buttons }, { quoted: m })
