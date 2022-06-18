af3ab1ace067ed93f9acdd481583466dec064464
Rʟxғʟʏ⁴̅⁰͍⁴̵ <87053694+Rlxfly@users.noreply.github.com>
2022-06-18 22:22:21 +0700

    Update _res.js

--- plugins/_res.js
+++ plugins/_res.js
@@ -1,21 +1,33 @@
+import fetch from "node-fetch"
+import { readFileSync } from "fs"
+
+
+
 export async function all(m) {
 
-  const stc = ['http://short.up.railway.app/qYCKWq',
-              'http://short.up.railway.app/jOgajy',
-              'http://short.up.railway.app/kEBa2O',
-              'http://short.up.railway.app/ZI4QQk',
-              'http://short.up.railway.app/uWMEQs',
-              'http://short.up.railway.app/aSyMjP']
-  var stcs = stc[Math.floor(Math.random() * (stc.length))]
+  //Kalo mau menggokil pake ini
+ // let pp = await this.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+
+  let stc = readFileSync('./tag.webp')
+
 if (m.isBaileys) return
     if (m.chat.endsWith('broadcast')) return
 
-    // when tagged send sticker 
+    // ketika ditag 
     try {
         if (m.mentionedJid.includes(this.user.jid) && m.isGroup) {
-            await this.sendFile(m.chat, stcs, 'tag.webp', '', m, false, {sendEphemeral: true})
+            await this.sendMessage(m.chat, { sticker : stc, thumbnail: thumb , contextInfo:{  externalAdReply: { showAdAttribution: true,
+mediaType:  1,
+mediaUrl: 'https://wa.me/6283820073017',
+title: '「 ❔ 」',
+body: me,
+sourceUrl: 'http://github.com/Rlxfly', thumbnail: thumb
+  }
+ }}, { quoted: m })
         }
     } catch (e) {
         return
     }
+
+                                
 }
