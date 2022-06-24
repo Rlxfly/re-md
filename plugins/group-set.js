4445fc25dde24d886baa3b2665660d2ebe3181d0
Rʟxғʟʏ⁴̅⁰͍⁴̵ <87053694+Rlxfly@users.noreply.github.com>
2022-06-24 21:20:30 +0700

    Create group-set.js

---
+++ plugins/group-set.js
@@ -0,0 +1,21 @@
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    let isClose = { 
+        'open': 'not_announcement',
+        'close': 'announcement',
+    }[(args[0] || '')]
+    if (isClose === undefined)
+        throw `
+*Format salah! Contoh :*
+  *${l}❌${r} ${usedPrefix + command} close*
+  *${l}✅${r} ${usedPrefix + command} open*
+`.trim()
+    await conn.groupSettingUpdate(m.chat, isClose)
+}
+handler.help = ['group *open / close*']
+handler.tags = ['group']
+handler.command = /^(group)$/i
+
+handler.admin = true
+handler.botAdmin = true
+
+export default handler
