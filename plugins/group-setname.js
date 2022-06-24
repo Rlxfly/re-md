385014a1ffb15b32873b0f3a9da4f6d9fc6b7015
Rʟxғʟʏ⁴̅⁰͍⁴̵ <87053694+Rlxfly@users.noreply.github.com>
2022-06-24 21:22:11 +0700

    Create group-setname.js

---
+++ plugins/group-setname.js
@@ -0,0 +1,16 @@
+let handler  = async (m, { conn, args, text }) => {
+if (!text) throw `Masukkan teks`
+try {
+let text = args.join` `
+if(!args || !args[0]) {
+} else {
+conn.groupUpdateSubject(m.chat, text)}
+} catch (e) {
+throw 'Erorr :('
+}}
+handler.help = ['setname <text>']
+handler.tags = ['group']
+handler.command = /^(setname)$/i
+handler.group = true
+handler.admin = true
+export default handler
