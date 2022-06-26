36a80355d818af186bcfb54779a8b6f3f181d597
Rʟxғʟʏ⁴̅⁰͍⁴̵ <87053694+Rlxfly@users.noreply.github.com>
2022-06-26 20:52:15 +0700

    All In

---
+++ plugins/_func.js
@@ -0,0 +1,207 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
+import moment from 'moment-timezone'
+
+let handler = m => m
+handler.all = async function (m) {
+    let name = await conn.getName(m.sender) 
+	let pp = 'https://telegra.ph/file/fc5e41b96c7a809b150c1.png'
+	try {
+		pp = await this.profilePictureUrl(m.sender, 'image')
+	} catch (e) {
+	} finally {
+		
+        //global.bg = await (await fetch(img)).buffer()
+		global.doc = pickRandom(["application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/msword", "application/pdf"])
+		
+		// Module 
+		global.fetch = import('node-fetch')
+		global.bochil = import('@bochilteam/scraper')
+		
+		const _uptime = process.uptime() * 1000
+        
+		// Ini untuk command crator/owner
+		global.kontak2 = [
+         [owner[0], await this.getName(owner[0] + '@s.whatsapp.net'), 'ᴅᴇᴠᴇʟᴏᴩᴇʀ ʙᴏᴛ', 'no@gmail.com', true],
+         [owner[1], await this.getName(owner[1] + '@s.whatsapp.net'), 'ᴅᴇᴠᴇʟᴏᴩᴇʀ ʙᴏᴛ', 'rlxfly.uwu@gmail.com', true], // Kalo mau di tambah tinggal copy 1baris ini di tempel di bawahnya trs di edit dikit!
+        ]
+        
+		// ucapan ini mah
+		global.ucapan = ucapan()
+		
+		// pesan sementara
+		global.ephemeral = '86400' // 86400 = 24jam, kalo ingin di hilangkan ganti '86400' jadi 'null' atau ''
+		
+		// externalAdReply atau text with thumbnail. gatau bahasa Inggris? coba translate!
+		global.adReply = {
+			contextInfo: {
+				forwardingScore: 9999,
+				//isForwarded: true, // ini biar ada tulisannya diteruskan berkali-kali, jika ingin di hilangkan ganti true menjadi false
+				externalAdReply: { // Bagian ini sesuka kalian berkreasi :'v
+                    showAdAttribution: true,
+					title: me,
+					body: "Hello " + name,
+					mediaUrl: 'https://tinyurl.com/29rt6ynv',
+					description: '「 ꜱɪᴍᴩʟᴇ ᴡʜᴀᴛꜱᴀᴩᴩ ʙᴏᴛ 」',
+					previewType: "PHOTO",
+					thumbnail: await (await fetch(pp)).buffer(),
+					sourceUrl: "https://github.com/Rlxfly",					
+				}
+			}
+		}
+		global.fakeig = {
+         contextInfo: { externalAdReply: { showAdAttribution: true,
+            mediaUrl: "https://Instagram.com/rlxfly.uw",
+            mediaType: "VIDEO",
+            description: "https://Instagram.com/rlxfly.uw", 
+            title: 'Simple Bot Esm',
+            body: me,
+            thumbnailUrl: pp,
+            sourceUrl: 'http://s.id/0x404'
+    }
+    } }
+		// Fake 🤥
+		global.ftroli = { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { itemCount: 9999999999999999999999999999999999999999999999999999999, status: 1, surface: 1, message: me, orderTitle: me, sellerJid: '0@s.whatsapp.net' } } }
+		global.fkontak = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': me, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${me},;;;\nFN:${me},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': fs.readFileSync('./me.png'), thumbnail: fs.readFileSync('./me.png'),sendEphemeral: true}}}
+        global.fvn = {
+            key: { 
+                 fromMe: false,
+                 participant: `0@s.whatsapp.net`, ...(m.chat ? 
+            { remoteJid: "17608914335-1625305606@g.us" } : {}) 
+                       },
+            message: { 
+               "audioMessage": {
+                        "mimetype":"audio/ogg; codecs=opus",
+                        "seconds": "999999999999",
+                        "ptt": "true"
+                               }
+                             } 
+                            }
+               
+                global.ftextt = {
+            key: { 
+                 fromMe: false,
+                 participant: `0@s.whatsapp.net`, ...(m.chat ? 
+            { remoteJid: "17608914335-1625305606@g.us" } : {}) 
+                       },
+            message: { 
+               "extendedTextMessage": {
+                        "text":me,
+                        "title": me,
+                        'jpegThumbnail': fs.readFileSync('./me.png')
+                               }
+                             } 
+                            }
+               
+                  global.fliveLoc = {
+            key:
+            { fromMe: false,
+            participant: `0@s.whatsapp.net`, ...(m.chat  ? 
+            { remoteJid: "status@broadcast" } : {}) },
+            message: { "liveLocationMessage": { "caption":"Hi","h": `${me}`, 'jpegThumbnail': fs.readFileSync('./me.png')}}
+           }
+               
+                  global.fliveLoc2 = {
+            key:
+            { fromMe: false,
+            participant: `0@s.whatsapp.net`, ...(m.chat ? 
+            { remoteJid: "status@broadcast" } : {}) },
+            message: { "liveLocationMessage": { "title": "Hi There Im Using GitHub","h": me, 'jpegThumbnail': fs.readFileSync('./me.png')}}
+           }
+               
+                   global.ftoko = {
+       key: {
+                   fromMe: false,
+                   participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "17608914335@s.whatsapp.net" } : {})
+               },
+               message: {
+                   "productMessage": {
+                       "product": {
+                           "productImage":{
+                               "mimetype": "image/jpeg",
+                               "jpegThumbnail": fs.readFileSync('./me.png') //Gambarnye
+                           },
+                           "title": me, //Kasih namalu 
+                           "description": "Simple Bot Esm", 
+                           "currencyCode": "USD",
+                           "priceAmount1000": "20000000",
+                           "retailerId": "Ghost",
+                           "productImageCount": 1
+                       },
+                           "businessOwnerJid": `0@s.whatsapp.net`
+               }
+           }
+       }
+               
+                     global.fdocs = {
+           key : {
+                  participant : '0@s.whatsapp.net'
+                               },
+              message: {
+                           documentMessage: {
+                           title: me, 
+                           jpegThumbnail: fs.readFileSync('./me.png')
+                                 }
+                               }
+                             }
+               
+                    global.fgclink = {
+           "key": {
+               "fromMe": false,
+               "participant": "0@s.whatsapp.net",
+               "remoteJid": "0@s.whatsapp.net"
+           },
+           "message": {
+               "groupInviteMessage": {
+                   "groupJid": "17608914335-1625305606@g.us",
+                   "inviteCode": "null",
+                   "groupName": "Iyh", 
+                   "caption": me, 
+                   'jpegThumbnail': fs.readFileSync('./me.png')
+               }
+           }
+       }
+       
+                    global.fgif = {
+            key: { 
+                 fromMe: false,
+                 participant: `0@s.whatsapp.net`, ...(m.chat ? 
+            { remoteJid: "17608914335-1625305606@g.us" } : {}) 
+                       },
+            message: { 
+                        "videoMessage": { 
+                        "title": me,
+                        "h": `Hmm`,
+                        'seconds': '999999999', 
+                        'gifPlayback': 'true', 
+                        'caption': me,
+                        'jpegThumbnail': fs.readFileSync('./me.png')
+                               }
+                              }
+                             }
+	}
+}
+
+export default handler 
+
+function ucapan() {
+    const time = moment.tz('Asia/Jakarta').format('HH')
+    let res = "Selamat malam 🌙"
+    if (time >= 4) {
+        res = "Selamat pagi 🌄"
+    }
+    if (time > 10) {
+        res = "Selamat siang ☀️"
+    }
+    if (time >= 15) {
+        res = "Selamat sore 🌅"
+    }
+    if (time >= 18) {
+        res = "Selamat malam 🌙"
+    }
+    return res
+}
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
