6a10891d8e149c13efaec6678420a63960a45824
Rʟxғʟʏ⁴̅⁰͍⁴̵ <87053694+Rlxfly@users.noreply.github.com>
2022-06-26 20:56:06 +0700

    Create group-revoke.js

---
+++ plugins/group-revoke.js
@@ -0,0 +1,26 @@
+import fetch from "node-fetch"
+
+
+let handler = async (m, { conn }) => {
+ 
+  let rest = await conn.groupRevokeInvite(m.chat)
+  let linked = 'https://chat.whatsapp.com/' + rest
+ let s = await shortUrl(linked)
+  
+		m.reply(s)
+  
+}
+handler.help = ['revoke']
+handler.tags = ['group']
+handler.command = /^re(voke|new)(invite|link)?$/i
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
+
+export default handler
+
+
+async function shortUrl(url) {
+	let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
+	return await res.text()
+}
