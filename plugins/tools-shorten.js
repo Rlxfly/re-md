84a1e7064cb125541451d5b01f504e2693030922
Rell <87053694+Rlxfly@users.noreply.github.com>
2022-08-13 20:43:26 +0700

    api shorten 😁
    😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁😁

---
+++ plugins/tools-shorten.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+let handler = async ( m, { conn, args } ) => {
+
+  if (!/^https?:\/\//.test(args[0])) throw 'Param *URL* must be starts with http:// or https://'
+  
+if(!args[0]) throw `Url?\n\nEx: .shorten http://github.com/Rlxfly Re7`
+
+let form = new URLSearchParams()
+form.append('url', args[0])
+form.append('code', args[1] || '')
+
+let res = await axios.post('https://s.rlxfly.my.id/', form)
+
+ await m.reply(res.data.url)
+}
+
+handler.help = ['shorten']
+handler.tags = ['tools']
+handler.command = /^short(en|link|url)?$/i
+
+export default handler
