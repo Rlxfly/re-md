5465e9e9298b46db8c24e69ccc64a598db9b9eaa
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-08-31 11:12:20 +0700

    📌fix

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,14 +1,11 @@
-import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
+import instagramGetUrl from 'instagram-url-direct'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`
+    
+    await m.reply(`${l}🔄${r}Ｌｏａｄｉｎｇ．．．`)
+    const results = (await instagramGetUrl(args[0])).url_list[0]
 
-  await m.reply(`${l}🔄${r}Ｌｏａｄｉｎｇ．．．`)
-    const results = await instagramdl(args[0])
-        .catch(async _ => await instagramdlv2(args[0]))
-        .catch(async _ => await instagramdlv3(args[0]))
-        .catch(async _ => await instagramdlv4(args[0]))
-    for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `${l}✅${r} Done\n${l}🔗${r} Url: ${url}`, m)
+    conn.sendFile(m.chat, results, 'instagram.mp4', `『 - - - - - ɪɴsᴛᴀɢʀᴀᴍ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ - - - - - 』*`, m)
 }
 handler.help = ['ig'].map(v => v + ' <url>')
 handler.tags = ['downloader']
