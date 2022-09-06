e6c753c77ba02517e6b65e12d0c9433e09a920e9
Rell <me@rlxfly.my.id>
2022-09-06 13:39:32 +0700

    gtw

--- plugins/misc-delete.js
+++ plugins/misc-delete.js
@@ -3,11 +3,11 @@ if (!m.quoted) throw 'Reply pesan yang ingin dihapus'
 try {
 let gada = m.message.extendedTextMessage.contextInfo.participant
 let mana = m.message.extendedTextMessage.contextInfo.stanzaId
-return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: mana, participant: gada }})
- } catch {
-return conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
-}
-}
+return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: mana, participant: gada }})}
+ catch {
+  return conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
+}}
+
 handler.help = ['del', 'delete']
 
 handler.command = /^d(el(ete)?$/i
