bdf3cd1755521dcf475521711ab8c4a7b2a3de29
Alvinocaharles <54900965+alvino-prog@users.noreply.github.com>
2022-10-06 17:08:58 +0700

    Update tools-tourl.js

--- plugins/tools-tourl.js
+++ plugins/tools-tourl.js
@@ -1,22 +1,17 @@
-import axios from 'axios'
-import clph from 'caliph-api'
-import fetch from "node-fetch"
-
 import uploadImage from '../lib/uploadImage.js'
 
-let handler = async (m) => {
+let handler = async (m, { conn, command, usedPrefix }) => {
 	let q = m.quoted ? m.quoted : m
 	let mime = (q.msg || q).mimetype || q.mediaType || ''
+	if (!mime) throw `Kirim foto/video yang ingin diubah ke url dengan caption *${usedPrefix}${command}* atau reply medianya`
 	let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
 	let media = await q.download()
-	if (isTele && media.length < 5242880) {
-		let data = await uploadImage(media)
-		m.reply(data)
-	} else if (/image|video|audio|sticker|document/.test(mime)) {
-		let data = await clph.tools.uploadFile(media),
-			shorten = await shortUrl(data.result.url)
-		m.reply(shorten)
-	} else throw 'No media found'
+	let link = await (isTele ? uploadImage : uploadFile)(media)
+	m.reply(`${conn.user.name}
+
+${link}
+${media.length} Byte(s)
+${isTele ? '(Tidak Ada Tanggal Kedaluwarsa)' : '(Tidak diketahui)'}`)
 }
 handler.help = ['tourl']
 handler.tags = ['tools']
@@ -24,7 +19,4 @@ handler.command = /^(upload|tourl)$/i
 
 export default handler
 
-async function shortUrl(url) {
-	let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
-	return await res.text()
-}
+
