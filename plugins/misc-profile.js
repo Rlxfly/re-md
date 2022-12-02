16a78d85d80183475814b9b4878354e4ce3c9532
Xiao Yan? <102222065+ImYanXiao@users.noreply.github.com>
2022-12-02 17:08:00 +0700

    Parel pemalas🗿

--- plugins/misc-profile.js
+++ plugins/misc-profile.js
@@ -1,31 +1,32 @@
 import PhoneNumber from 'awesome-phonenumber'
-import moment from 'moment-timezone'
-
-let handler = async (m, { conn, usedPrefix }) => {
+import * as levelling from '../lib/levelling.js'
+let handler = async (m, { conn }) => {
+  let pp = thumb2
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  var { status, setAt } = await conn.fetchStatus(who).catch(() => {
-          return {
-            status: "",
-            setAt: "",
-          };
-        });
-   let pp
   try {
-    pp = await conn.profilePictureUrl(who, "image")
+    pp = await conn.profilePictureUrl(who, 'image')
   } catch (e) {
-    pp = "https://telegra.ph/file/e47d9ec693e5288ad9382.jpg"
+
   } finally {
+    let about = (await conn.fetchStatus(who).catch(console.error) || {}).status || ''
+    let { name, limit, exp, lastclaim, registered, regTime, age, level } = global.DATABASE.data.users[m.sender]
+    let { min, xp, max } = levelling.xpRange(level, global.multiplier)
     let username = conn.getName(who)
     let str = `
-• Name: ${username}, \n• Tag: @${who.replace(/@.+/, '')}, ${status ? '\n• Bio: ' + status : ''}, \n• Set At Bio: ${(setAt && moment(setAt).format("DD MMMM YYYY")) || "Unknown"}, \n• Number: ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')},\n• Link: https://wa.me/${who.split`@`[0]}`.trim()
-
+Name: ${username} ${registered ? '(' + name + ') ': ''}(@${who.replace(/@.+/, '')})${about ? '\nAbout: ' + about : ''}
+Number: ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+Link: https://wa.me/${who.split`@`[0]}${registered ? '\nAge: ' + age : ''}
+XP: TOTAL ${exp} (${exp - min} / ${xp}) [${max - exp} left to levelup]
+Level: ${level}
+Limit: ${limit}
+Registered: ${registered ? 'Yes (' + new Date(regTime) + ')': 'No'}${lastclaim > 0 ? '\nLast Claim: ' + new Date(lastclaim) : ''}
+`.trim()
     let mentionedJid = [who]
-    conn.sendFile(m.chat, pp, 'pp.jpeg', str, m, false, { contextInfo: { mentionedJid }})
+    conn.sendFile(m.chat, pp, 'pp.jpg', str, m, false, { contextInfo: { mentionedJid }})
   }
 }
 handler.help = ['profile [@user]']
 handler.tags = ['tools']
-handler.command = /^(profile)$/i
-handler.group = true
+handler.command = /^profile$/i
 
 export default handler
