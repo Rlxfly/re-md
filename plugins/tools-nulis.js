abe6a76cd6cd47a2d9bbb0b9476c1d33acbc8d66
Re7Pntx <me@rlxfly.my.id>
2023-01-26 11:22:57 +0700

    re upload

---
+++ plugins/tools-nulis.js
@@ -0,0 +1,73 @@
+import util from 'util'
+import path from 'path'
+
+import { spawn } from 'child_process'
+
+// uwu
+let fontPath = 'src/font/Zahraaa.ttf'
+let handler  = async (m, { conn, args }) => {
+  let inputPath ='src/kertas/magernulis1.jpg'
+  let outputPath = 'tmp/hasil.jpg'
+  let d = new Date
+  let tgl = d.toLocaleDateString('id-Id')
+  let hari = d.toLocaleDateString('id-Id', { weekday: 'long' })
+  let teks = args.join` `
+  // conn.reply(m.chat, util.format({fontPath, inputPath, outputPath, tgl, hari, teks}), m)
+  spawn('convert', [
+    inputPath,
+    '-font',
+    fontPath,
+    '-size',
+    '1024x784',
+    '-pointsize',
+    '20',
+    '-interline-spacing',
+    '1',
+    '-annotate',
+    '+806+78',
+    hari,
+    '-font',
+    fontPath,
+    '-size',
+    '1024x784',
+    '-pointsize',
+    '18',
+    '-interline-spacing',
+    '1',
+    '-annotate',
+    '+806+102',
+    tgl,
+    '-font',
+    fontPath,
+    '-size',
+    '1024x784',
+    '-pointsize',
+    '20',
+    '-interline-spacing',
+    '-7.5',
+    '-annotate',
+    '+344+142',
+    teks,
+    outputPath
+  ])
+  .on('error', e => conn.reply(m.chat, util.format(e), m))
+  .on('exit', () => {
+    conn.sendFile(m.chat, outputPath, 'nulis.jpg', 'Hati² ketahuan:v', m)
+  })
+}
+
+handler.help = ['n'].map(v => v + 'ulis <teks>')
+handler.tags = ['nulis']
+handler.command = /^nulis$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+export default handler
